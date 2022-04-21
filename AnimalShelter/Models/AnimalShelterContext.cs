77977bdac4924ead16d5ce4226837033d90a2179
Nick Sullivan <nicholassull@gmail.com>
2022-04-21 15:24:49 -0700

    add AnimalShelterContext class and constructor

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options) : base(options)
+    {
+    }
+  }
+}
