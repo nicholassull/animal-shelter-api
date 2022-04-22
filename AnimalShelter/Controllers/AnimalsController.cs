5fce28e0dbd76ed85aefda33b71f9d7cc4bf767d
Nick Sullivan <nicholassull@gmail.com>
2022-04-22 14:43:20 -0700

    add _db field and constructor for AnimalsController

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -3,4 +3,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using CretaceousPark.Models;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+  }
+}
