3b7591ed0ffee82a2ff67471d0ace753a682f4d1
Nick Sullivan <nicholassull@gmail.com>
2022-04-22 14:41:50 -0700

    add base for AnimalsController.cs

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using CretaceousPark.Models;
